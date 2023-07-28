961bfd07e213bd93cb17d758cc857cf83156d8b2
hossy21 <95125392+u-hossy@users.noreply.github.com>
2023-07-28 17:55:06 +0900

    イベントリスナーの試験追加

--- script.js
+++ script.js
@@ -1,4 +1,16 @@
 // window.confirm("今から晴れるよ");
+document.addEventListener('keydown', keydown);
+
+function player1_ans() {
+    msg.innerText = 'プレイヤー1(f)さん、お答えください！';
+}
+
+function player2_ans() {
+    msg.innerText = 'プレイヤー2(g)さん、お答えください！';
+}
+
+
+
 let msg = document.getElementById('status_msg');
-msg.innerText = '解答がわかったらボタンを押そう！！';
+msg.innerText = '準備完了：解答がわかったらボタンを押そう！！';
 
